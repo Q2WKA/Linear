06d6ea464b3e0655750a9050d161a72ca242a0bb
Renat Kutushev <93474253+Q2WKA@users.noreply.github.com>
2023-02-12 23:16:28 +0300

    tests for gaussian and rotation invertors

--- Linear.cpp
+++ Linear.cpp
@@ -6,23 +6,39 @@
 
 using namespace utils;
 
-int main() {
-	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+int main(int argc, char* argv[]) {
+	std::chrono::steady_clock::time_point begin1 = std::chrono::steady_clock::now();
 
-	Matrix to_invert = Matrix(100, 100, &revAbsDiff);
-	Matrix copied = to_invert;
+	Matrix to_invert1 = Matrix(1000, 1000, &revAbsDiff);
 
-	GaussianInverter gaussianInverter(std::make_shared<Matrix>(to_invert));
+	GaussianInverter gaussianInverter(std::make_shared<Matrix>(to_invert1));
 	gaussianInverter.directAlgorithm();
 	gaussianInverter.reverseGauss();
-	gaussianInverter.checkRes(copied);
+	// gaussianInverter.checkRes(to_invert);
 
 	std::unique_ptr<Matrix> res = gaussianInverter.returnResult();
 	std::cout << std::endl << "Inverted matrix: " << std::endl;
-	res.get()->printTrunkated(10);
+	res.get()->printTrunkated(10, 1. / 100);
 
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[micro seconds]" << std::endl;
+	std::chrono::steady_clock::time_point end1 = std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end1 - begin1).count() << "[micro seconds]" << std::endl;
+
+
+
+
+	std::chrono::steady_clock::time_point begin2 = std::chrono::steady_clock::now();
+
+	RotationInverter rotationInverter(std::make_shared<Matrix>(to_invert1));
+	rotationInverter.directAlgorithm();
+	rotationInverter.reverseGauss();
+	// gaussianInverter.checkRes(to_invert);
+
+	res = rotationInverter.returnResult();
+	std::cout << std::endl << "Inverted matrix: " << std::endl;
+	res.get()->printTrunkated(10, 1. / 100);
+
+	std::chrono::steady_clock::time_point end2 = std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end2 - begin2).count() << "[micro seconds]" << std::endl;
 
     return 0;
 }
