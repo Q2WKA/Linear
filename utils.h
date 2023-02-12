0dc8697b13da0322a8cc6b2abf6672f3f4e0625b
Renat Kutushev <93474253+Q2WKA@users.noreply.github.com>
2023-02-12 23:16:47 +0300

    added quake inverse square root classic implementation

--- utils.h
+++ utils.h
@@ -1,11 +1,23 @@
 #pragma once
+#include <cstdint>
 #include <memory>
 #include <vector>
 #include <string>
+#include <limits>
+#include <bit>
 
 
 namespace utils {
     constexpr double EPS = 1e-16;
+
+    constexpr float Q_rsqrt(float number) noexcept
+    {
+        static_assert(std::numeric_limits<float>::is_iec559); // (enable only on IEEE 754)
+
+        float const y = std::bit_cast<float>(
+            0x5f3759df - (std::bit_cast<std::uint32_t>(number) >> 1));
+        return y * (1.5f - (number * 0.5f * y * y));
+    }
 
     inline double e(int i, int j) {
         return (i == j);
